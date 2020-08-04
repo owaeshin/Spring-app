2823d0dedec8200caa82df49b3f3a819761352ba
at.kurbanov <at.kurbanov@gmail.com>
2020-08-04 22:13:43 +0300

    lesson 6 DI via setter

--- src/main/java/ru/kurbanov/springcourse/MusicPlayer.java
+++ src/main/java/ru/kurbanov/springcourse/MusicPlayer.java
@@ -3,8 +3,33 @@ package ru.kurbanov.springcourse;
 public class MusicPlayer {
 
     private Music music;
+    private String name;
+    private Integer volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
