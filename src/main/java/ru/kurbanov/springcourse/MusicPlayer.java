b4344dff15d49bbcf6c1f3ad35cc302abe1cc024
at.kurbanov <at.kurbanov@gmail.com>
2020-08-04 22:24:19 +0300

    lesson 6 HomeWork with list

--- src/main/java/ru/kurbanov/springcourse/MusicPlayer.java
+++ src/main/java/ru/kurbanov/springcourse/MusicPlayer.java
@@ -1,8 +1,11 @@
 package ru.kurbanov.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private Integer volume;
 
@@ -22,18 +25,11 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        this.musicList.forEach(m -> System.out.println(m.getSong()));
     }
 }
