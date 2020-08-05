b38ca19832aeb4b4a0560a000f5f0b1e68d425a8
at.kurbanov <at.kurbanov@gmail.com>
2020-08-05 11:00:12 +0300

    lesson 8 init destroy

--- src/main/java/ru/kurbanov/springcourse/ClassicMusic.java
+++ src/main/java/ru/kurbanov/springcourse/ClassicMusic.java
@@ -1,6 +1,22 @@
 package ru.kurbanov.springcourse;
 
 public class ClassicMusic implements Music {
+
+    private ClassicMusic() {
+    }
+
+    public static ClassicMusic getClassicalMusic() {
+        return new ClassicMusic();
+    }
+
+    public void init() {
+        System.out.println("Doing init");
+    }
+
+    public void destroy() {
+        System.out.println("Doing destroy");
+    }
+
     @Override
     public String getSong() {
         return "Classic";
--- src/main/java/ru/kurbanov/springcourse/TestSpring.java
+++ src/main/java/ru/kurbanov/springcourse/TestSpring.java
@@ -8,14 +8,17 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-//        Music music = context.getBean("musicBean", Music.class);
+        ClassicMusic classicMusic = context.getBean("musicBean", ClassicMusic.class);
 
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(classicMusic.getSong());
+        System.out.println(classicMusic.getSong());
 
-        musicPlayer1.playMusic();
-
-        System.out.println(musicPlayer1 == musicPlayer2);
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        musicPlayer1.playMusic();
+//
+//        System.out.println(musicPlayer1 == musicPlayer2);
 
         context.close();
     }
